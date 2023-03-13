5a7d8b988b7a8e661ea7c37be2a88818d44835a2
SvendBD <sdsvend@gmail.com>
2023-03-13 12:31:55 +0100

    Initial Commit

---
+++ src/model/Product.java
@@ -0,0 +1,12 @@
+package model;
+
+public class Product {
+	private String name;
+	private String brand;
+	private int price;
+	private String countryOfOrigen;
+	private int minStock;
+	private int stock;
+	private long id;
+	private String description;
+}
