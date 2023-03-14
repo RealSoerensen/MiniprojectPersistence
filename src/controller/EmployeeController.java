65ca18595e631849c5fe5cbe77a433bfae928e1b
Patrick Sørensen <62727344+RealSoerensen@users.noreply.github.com>
2023-03-14 12:49:13 +0100

    removed login from employeectrl

--- src/controller/EmployeeController.java
+++ src/controller/EmployeeController.java
@@ -3,10 +3,4 @@ package controller;
 import dal.employee.EmployeeContainer;
 
 public class EmployeeController implements EmployeeCtrIF {
-
-	@Override
-	public boolean login(String username, String password) {
-		return EmployeeContainer.getInstance().login(username, password);
-	}
-
 }
